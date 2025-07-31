26df682643c48476b4294d6b08174a5bbac3f161
Evan Clark <black0ashes@gmail.com>
2025-07-31 21:36:24 +0700

    L5 (10.3)

--- 331-l2/src/App.vue
+++ 331-l2/src/App.vue
@@ -9,14 +9,14 @@ const { message } = storeToRefs(store)
 <template>
   <div class="text-center font-sans text-gray-700 antialias">
     <header>
-      <div id="flashMessage" v-if="message">
+      <div id="flashMessage" class="animate-fade" v-if="message">
         <h4>{{ message }}</h4>
       </div>
       <div class="wrapper">
-        <nav>
-          <RouterLink :to="{ name: 'event-list-view' }">Event</RouterLink> |
-          <RouterLink :to="{ name: 'about' }">About</RouterLink> |
-          <RouterLink :to="{ name: 'Students' }">Students</RouterLink>
+        <nav class=""py-6>
+          <RouterLink class="font-bold text-gray-700" exact-active-class="text-green-500" :to="{ name: 'event-list-view' }">Event</RouterLink> |
+          <RouterLink class="font-bold text-gray-700" exact-active-class="text-green-500" :to="{ name: 'about' }">About</RouterLink> |
+          <RouterLink class="font-bold text-gray-700" exact-active-class="text-green-500" :to="{ name: 'Students' }">Students</RouterLink>
         </nav>
       </div>
     </header>
@@ -41,15 +41,6 @@ nav a.router-link-exact-active {
 
 h2 {
   font-size: 20px;
-}
-
-@keyframes yellofade {
-  from {
-    background-color: yellow;
-  }
-  to {
-    background-color: transparent;
-  }
 }
 
 #flashMessage {
